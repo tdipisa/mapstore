9d4642974f11e9912f0262950424c51aa947c3c2
tobia <tobia.dipisa@geo-solutions.it>
2013-08-30 19:27:33 +0200

    Fixes for gxp plugins after the OL upgrade

--- mapcomposer/app/static/config/development4326.js
+++ mapcomposer/app/static/config/development4326.js
@@ -8,67 +8,24 @@
 			"title": "Local GeoServer",
 			"url": "http://localhost:8080/geoserver/ows",
 			"version":"1.1.1",
-		    "layersCachedExtent": [
-				-20037508.34,-20037508.34,
-				20037508.34,20037508.34
-			],
 			"layerBaseParams":{
 				"FORMAT":"image/png8",
-				"TILED":true
+				"TILED":true,
+				"TILESORIGIN": "-180, -90"
 			}
-		},
-		"mapquest": {
-			"ptype": "gxp_mapquestsource"
-		}, 
-		"osm": { 
-			"ptype": "gxp_osmsource"
-		},
-		"google": {
-			"ptype": "gxp_googlesource" 
-		},
-		"bing": {
-			"ptype": "gxp_bingsource" 
-		}, 
-		"ol": { 
-			"ptype": "gxp_olsource" 
 		}
 	},
 	"map": {
-		"projection": "EPSG:900913",
+		"projection": "EPSG:4326",
 		"units": "m",
 		"extent": [
-			1256194.1026318,5855470.2312771,1269465.481666,5862722.1943347
+			-124.73142200000,24.955967,-66.969849,49.371735
 		],
 		"layers": [
 			{
-				"source": "bing",
-				"title": "Bing Aerial",
-				"name": "Aerial",
-				"group": "background"
-			},{
-				"source": "osm",
-				"title": "Open Street Map",
-				"name": "mapnik",
-				"group": "background"
-			},{
-				"source": "mapquest",
-				"title": "MapQuest OpenStreetMap",
-				"name": "osm",
-				"group": "background"
-			},{
-				"source": "google",
-				"title": "Google Roadmap",
-				"name": "ROADMAP",
-				"group": "background"
-			},{
-				"source": "google",
-				"title": "Google Terrain",
-				"name": "TERRAIN",
-				"group": "background"
-			},{
-				"source": "google",
-				"title": "Google Hybrid",
-				"name": "HYBRID",
+				"source": "local",
+				"title": "Blue Marble",
+				"name": "it.geosolutions:blue_marble",
 				"group": "background"
 			}
 		]
